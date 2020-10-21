3700c385ce82455fca020153c48f326eb9327dd0
mugenga <rockyyves@gmail.com>
2020-10-21 22:47:23 +0200

    added a web

---
+++ build/precache-manifest.2d8b9063672014a7e62952a04d4969d5.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2255484d49126eb4c02",
+    "url": "/build/static/css/main.20fa8304.chunk.css"
+  },
+  {
+    "revision": "b2255484d49126eb4c02",
+    "url": "/build/static/js/main.4e5cf184.chunk.js"
+  },
+  {
+    "revision": "b4d8ef2cb7791815737b",
+    "url": "/build/static/js/runtime~main.9df7376b.js"
+  },
+  {
+    "revision": "4238eb506502891987f1",
+    "url": "/build/static/css/2.6358e177.chunk.css"
+  },
+  {
+    "revision": "4238eb506502891987f1",
+    "url": "/build/static/js/2.d32dde8f.chunk.js"
+  },
+  {
+    "revision": "79a1b1ccd95eb178096d862f31e57176",
+    "url": "/build/static/media/image-3.79a1b1cc.jpg"
+  },
+  {
+    "revision": "fc78c35ee9f3e35cd40101150e5472f7",
+    "url": "/build/static/media/image-1.fc78c35e.jpg"
+  },
+  {
+    "revision": "de5023535f5d580b2b81f7b02b8ea481",
+    "url": "/build/static/media/EANPaylogoW.de502353.jpg"
+  },
+  {
+    "revision": "5ed1979409ceb674c6248624e740113e",
+    "url": "/build/static/media/profile-thumb.5ed19794.jpg"
+  },
+  {
+    "revision": "05e8742b74546de5fad0975d810fa8f8",
+    "url": "/build/static/media/HIWIOS.05e8742b.svg"
+  },
+  {
+    "revision": "9c1f1e9ee2d7a60351d3ea52d2b2ec4a",
+    "url": "/build/static/media/app.9c1f1e9e.png"
+  },
+  {
+    "revision": "53ffe2c1095d713da2d2f635b90f8e4f",
+    "url": "/build/static/media/RecipientsIcon.53ffe2c1.svg"
+  },
+  {
+    "revision": "0164e78ae3a3022cebc7a141520bcfb3",
+    "url": "/build/static/media/CreditCardIcon.0164e78a.svg"
+  },
+  {
+    "revision": "146c55ced5bc9f6945cac9fcc4cefb37",
+    "url": "/build/static/media/ProfileIcon.146c55ce.svg"
+  },
+  {
+    "revision": "99576239197e6b8273ddc67dad502b6d",
+    "url": "/build/static/media/ActivitiesIcon.99576239.svg"
+  },
+  {
+    "revision": "f7b16065c7ee27a1da252628355d3b31",
+    "url": "/build/static/media/logo.f7b16065.png"
+  },
+  {
+    "revision": "ae5b3e3107550b3e4813aaacc1ab066b",
+    "url": "/build/static/media/HIWPayConnect.ae5b3e31.svg"
+  },
+  {
+    "revision": "711920ed5f86b76ca49d063563c80e9c",
+    "url": "/build/static/media/HIWEanpayMoneyTransfer.711920ed.svg"
+  },
+  {
+    "revision": "5accad5fd868b0302412a84089a56b99",
+    "url": "/build/static/media/anyone-freelancer.5accad5f.jpg"
+  },
+  {
+    "revision": "ddb25d1467c0cf4462db88eb721ec5c8",
+    "url": "/build/static/media/anyone-online-shopping.ddb25d14.jpg"
+  },
+  {
+    "revision": "a4ce74017609e5f0b7bf6f338214eae4",
+    "url": "/build/static/media/anyone-online-sellers.a4ce7401.jpg"
+  },
+  {
+    "revision": "84af1544a310b93a4e4715c7f605b76f",
+    "url": "/build/static/media/anyone-affiliate-marketing.84af1544.jpg"
+  },
+  {
+    "revision": "a5b36b4fd5a6dae2e4a516e980995519",
+    "url": "/build/static/media/how-work.a5b36b4f.jpg"
+  },
+  {
+    "revision": "f21890ca4f5bba02fe90b8f002e8be69",
+    "url": "/build/static/media/image-2.f21890ca.jpg"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/build/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/build/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/build/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/build/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/build/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "d3cfa37e66279ae615d81275a6fd76e3",
+    "url": "/build/static/media/PayConnectHome.d3cfa37e.svg"
+  },
+  {
+    "revision": "103f4662674cbee5beb9e61d6120836b",
+    "url": "/build/static/media/HomeEanpayMoneyTransfer.103f4662.svg"
+  },
+  {
+    "revision": "60463f4a4af7ab9318ab8bea5bd298c6",
+    "url": "/build/static/media/HomeLowCost.60463f4a.svg"
+  },
+  {
+    "revision": "2e3774eff882b3eadfa9546c14b3f84c",
+    "url": "/build/static/media/HomeEasyAccess.2e3774ef.svg"
+  },
+  {
+    "revision": "caa14f940524840b5fcd06af86fbcf91",
+    "url": "/build/static/media/HomeHighBenefits.caa14f94.svg"
+  },
+  {
+    "revision": "e239de0c3522a5c2e1ce4cbfe4980f97",
+    "url": "/build/static/media/HomeFullySecure.e239de0c.svg"
+  },
+  {
+    "revision": "66865222c84b2e5539e874d5a9f15c21",
+    "url": "/build/static/media/HomeClientTrusted.66865222.svg"
+  },
+  {
+    "revision": "5fee1ffac47cbe7ed3f3b191fc6b6bdf",
+    "url": "/build/static/media/HomeFastTransition.5fee1ffa.svg"
+  },
+  {
+    "revision": "f29bb25410e1a850a743b6d36ebcc9ab",
+    "url": "/build/static/media/HomeKCB.f29bb254.svg"
+  },
+  {
+    "revision": "4866005563916342a98a4b04b8dca7c7",
+    "url": "/build/static/media/HomeGTBank.48660055.svg"
+  },
+  {
+    "revision": "655e83d113359e9fe49003160219716f",
+    "url": "/build/static/media/HomeEcobank.655e83d1.svg"
+  },
+  {
+    "revision": "5d39b51724789e1fc78ec9670f439366",
+    "url": "/build/index.html"
+  }
+];
